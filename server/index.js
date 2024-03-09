6010724d2974d6d49665b019f647851f76547564
Erfan Heshmati <heshmati74@gmail.com>
2024-03-09 11:42:20 +0330

    mongodb config

--- server/index.js
+++ server/index.js
@@ -5,11 +5,41 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-// database config
-require("./db");
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// MongoDB configuration
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Database and Collections
+    const menuCollection = client.db("foodi").collection("menus");
+    const cartCollection = client.db("foodi").collection("carts");
+    // Menu items operation
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("MongoDB connected successfully");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
