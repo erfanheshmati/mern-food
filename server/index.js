79e73b97e3c68162eba7b33500a35fc18bcba8c3
Erfan Heshmati <heshmati74@gmail.com>
2024-03-09 10:48:40 +0330

    mongodb config

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const app = express();
 
+// database config
+require("./db");
+
 // middlewares
 app.use(cors());
 app.use(express.json());
