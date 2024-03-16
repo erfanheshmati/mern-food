ba449493b5b56504105b96d7bf678f84df3cf47d
Erfan Heshmati <heshmati74@gmail.com>
2024-03-16 20:03:27 +0330

    jwt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
 const menuRoute = require("./routes/menuRoute");
 const cartRoute = require("./routes/cartRoute");
+const userRoute = require("./routes/userRoute");
+
 require("dotenv").config();
 require("./database");
 
@@ -12,8 +16,16 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
+// JWT Authentication
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1hr" });
+  res.send({ token });
+});
+
 // Routes
 app.use("/menu", menuRoute);
 app.use("/cart", cartRoute);
+app.use("/user", userRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
