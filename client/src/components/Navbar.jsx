a04787081e8518959fd2edf6d6fc75259cb65382
Erfan Heshmati <heshmati74@gmail.com>
2024-03-26 10:39:11 +0330

    bug fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,10 +11,8 @@ export default function Navbar() {
     const [isSticky, setIsSticky] = useState(false)
 
     const { user } = useAuth()
-    // console.log(user)
 
-    const [cart, refetch] = useCart()
-    // console.log(cart)
+    const [cart] = useCart()
 
     useEffect(() => {
         const handleScroll = () => {
@@ -106,10 +104,11 @@ export default function Navbar() {
                                     <div className="indicator">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                                         <span className="badge badge-sm indicator-item border-none bg-green text-white p-2">
-                                            {cart.length || 0}
+                                            {cart.length}
                                         </span>
                                     </div>
-                                </div></Link>
+                                </div>
+                            </Link>
                         </div>
                         {/* login btn */}
                         {user ? <Profile user={user} /> : (
